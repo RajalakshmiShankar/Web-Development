01e1e89143af1c35fdc30aff131744f5bc9da192
RajalakshmiShankar <laxmi.achu003@gmail.com>
2024-12-15 02:29:00 +0530

    adding files

---
+++ Website devp/React/280-jsx-code-challenge/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Ice Creams </li>
+      <li>Pinneapples </li>
+      <li>KFC</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
