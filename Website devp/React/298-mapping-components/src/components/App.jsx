79e7d16122ce4c456f33ff1cd5333fe095c3b50d
RajalakshmiShankar <laxmi.achu003@gmail.com>
2024-12-24 19:48:18 +0530

    adding files

--- Website devp/React/298-mapping-components/src/components/App.jsx
+++ Website devp/React/298-mapping-components/src/components/App.jsx
@@ -17,7 +17,7 @@ function createCard(contact) {
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
+      <h1 className='heading'>My Contacts</h1>
       {contacts.map(createCard)}
 
       {/* <Card
---
+++ Website devp/React/299-mapping-components-practice/src/emojipedia.js
@@ -0,0 +1,53 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits.",
+  },
+  {
+    id: 4,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude. Sometimes this emoji is used humorously to show a winking innocence for less good deeds or behaviors. ",
+  },
+  {
+    id: 5,
+    emoji: "🤓",
+    name: " Nerd Face",
+    meaning:
+      "Nerd emoji 🤓 is a face with big glasses, an awkward smile and buck teeth. It's often used in a humorous or ironic way to describe nerds or to express how smart you are. It represents the stereotype of the nerd, an intelligent but oddly dressed person with social deficits. ",
+  },
+  {
+    id: 6,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness. heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude. Sometimes this emoji is used humorously to show a winking innocence for less good deeds or behaviors. ",
+  },
+];
+
+export default emojipedia;
